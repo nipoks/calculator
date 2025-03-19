c9ec271befb07ac62d2c1980e516d0c4b4e0ec90
nipoks <skopin.nikita@list.ru>
2025-03-19 13:15:22 +0700

    ress

--- fe/src/common/AuthInterceptor.tsx
+++ fe/src/common/AuthInterceptor.tsx
@@ -6,13 +6,17 @@ import { RequestConfig, ServerError } from '../api/createRequest.ts'
 import { WithChildren } from './WithChildren.ts'
 import { useAuthContext } from '../context/authContext/authContext.ts'
 
+let currentInterceptor: number | null = null;
 
 export const AuthInterceptor: FC<WithChildren> = ({ children }) => {
     const { authClient, authState } = useAuthContext()
     const navigate = useNavigate()
 
     useMemo(() => {
-        axios.interceptors.request.use(async (config) => {
+        if (currentInterceptor !== null) {
+            axios.interceptors.request.eject(currentInterceptor)
+        }
+        currentInterceptor = axios.interceptors.request.use(async (config) => {
             const requestConfig = config as RequestConfig
             if (!requestConfig.isProtectRequest) {
                 return config
@@ -28,12 +32,15 @@ export const AuthInterceptor: FC<WithChildren> = ({ children }) => {
                     throw error
                 }
             }
+
             const accessToken = authState?.accessToken
             if (accessToken && config.headers) {
                 config.headers.Authorization = `Bearer ${accessToken}`
             }
+
             return config
         })
+
     }, [authState, authClient, navigate])
 
     return <>{children}</>
